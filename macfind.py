e80e490cae2e61888496de253a2c18c3c7b23c34
Shoichi Sakane <github-shoichi@tanu.org>
2018-04-01 00:02:24 +0900

    removed unused code.

--- macfind.py
+++ macfind.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-from __future__ import unicode_literals
 
 import argparse
 import requests
@@ -16,9 +15,9 @@ db_file = os.path.dirname(os.path.abspath(__file__)) + "/oui.txt"
 
 '''
 Usage:
-    this [-v] [-x] mac_addr
-    this [-v] -z mac_addr
-    this [-v] update
+    macfind mac_addr
+    macfind -z mac_addr
+    macfind update
 
 update: update the local database of OUI. other options are ignored.
 '''
@@ -35,10 +34,6 @@ def parse_args():
     p.add_argument("-z", action="store_true", dest="f_online",
                     help="specify online mode. it's going to ask %s." %
                     url_server)
-    p.add_argument("-x", action="store_true", dest="f_reverse",
-                    help="specify reverse lookup. the -z option is ignored.")
-    p.add_argument("-v", action="store_true", dest="f_verbose",
-                   default=False, help="enable verbose mode.")
     args = p.parse_args()
     args.mac_addr = " ".join(args._rest_args)
     return args
@@ -70,9 +65,10 @@ ma = ("-".join(["%s%s"%(ma[i],ma[i+1]) for i in range(0,6,2)])).upper()
 if sys.version_info.major < 3:
     ma = ma.encode(encoding="utf-8")    # for python2
 
+print("Searching for", ma)
+
 if opt.f_online == True:
     url = url_server + ma
-    print(ma)
     res = requests.get(url)
     if res.status_code == 200:
         print(res.text)
@@ -81,6 +77,10 @@ if opt.f_online == True:
     exit(0)
 
 with open(db_file, "r") as f:
+    count = 0
     for line in f:
         if ma == line[:8]:
             print(line, end="")
+            count += 1
+    if not count:
+        print("not found")
